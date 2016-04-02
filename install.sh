1141d8297689685bbdb5ad9e09625db4c993e17b
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2016-04-02 19:18:47 +0200

    Re-factoring for good :)
    Fix loop conditions: now they finally understand when the docker is not running!
    Hide all the outputs, the goal of this script is to make the install procedure noob-friendly.
    And because of this, now the scripts prints out the current steps rather than generic output of every command.

--- install.sh
+++ install.sh
@@ -2,25 +2,33 @@
 
 # Configure Domains
 RANCHER_DOMAIN="docker.lan"
+RANCHER_SERVER_NAME="rancher-server"
+RANCHER_AGENT_NAME="rancher-agent"
 MAILSERVER_DOMAIN="mail.lan"
+MAILSERVER_NAME="mail-server"
 
 # Install Docker
-yes '' | pacman -Sy --noprogressbar --noconfirm --needed docker openssl
+echo ">> Installing required packages..."
+yes '' | pacman -Sy --noprogressbar --noconfirm --needed docker openssl &>/dev/null
 
 # Enable and Start docker host service
-systemctl enable docker.service
-systemctl start docker.service
+echo ">> Enabling docker service..."
+systemctl enable docker.service &>/dev/null
+systemctl start docker.service &>/dev/null
 
 # Prepare the Automatic Reverse proxy manager certs folder
-mkdir -p /srv/certs
+echo ">> Creating /srv/certs folder..."
+mkdir -p /srv/certs &>/dev/null
 
 # Create self-signed certificate for mail server
+echo ">> Generating $MAILSERVER_DOMAIN self-signed certificate..."
 openssl req \
     -subj "/O=Mail Server/CN=$MAILSERVER_DOMAIN" \
     -newkey rsa:2048 -nodes -keyout "/srv/certs/$MAILSERVER_DOMAIN.key" \
-    -x509 -days 365 -out "/srv/certs/$MAILSERVER_DOMAIN.crt"
+    -x509 -days 365 -out "/srv/certs/$MAILSERVER_DOMAIN.crt" &>/dev/null
 
 # Install Automatic Reverse proxy manager
+echo ">> Running Reverse Proxy docker..."
 docker run \
     --restart=always \
     --name=docker-auto-reverse-proxy \
@@ -29,34 +37,41 @@ docker run \
     -p 443:443 \
     -v /srv/certs:/etc/nginx/certs \
     -v /var/run/docker.sock:/tmp/docker.sock:ro \
-    jwilder/nginx-proxy
+    jwilder/nginx-proxy &>/dev/null
 
 # Install Rancher Server
+echo ">> Running Rancher Server..."
 docker run \
     --restart=always \
-    --name=rancher-server \
+    --name="$RANCHER_SERVER_NAME" \
     -d \
-    -e "CATTLE_API_HOST=http://rancher-server:8080" \
+    -e "CATTLE_API_HOST=http://$RANCHER_SERVER_NAME:8080" \
     -e "VIRTUAL_HOST=$RANCHER_DOMAIN" \
     -e "VIRTUAL_PORT=8080" \
-    rancher/server:v1.0.0
+    rancher/server:v1.0.0 &>/dev/null
 
 # Register Rancher Agent
+echo ">> Running Rancher agent..."
 docker run \
-    --name=register-rancher-agent \
-    --link=rancher-server \
+    --name="register-$RANCHER_AGENT_NAME" \
+    --link=$RANCHER_SERVER_NAME \
     --privileged \
     -d \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /var/lib/rancher:/var/lib/rancher \
-    rancher/agent:v0.11.0 http://rancher-server:8080/v1
+    rancher/agent:v0.11.0 "http://$RANCHER_SERVER_NAME:8080/v1" &>/dev/null
 
 # Wait until the rancher agent is up and running
-until [ "$(/usr/bin/docker inspect -f {{.State.Running}} rancher-agent)"=="true" ]; do
-    sleep 0.1
+echo -n ">> Waiting for Rancher Agent to start..."
+while [ ! $(docker top $RANCHER_AGENT_NAME &>/dev/null && echo $?) ]
+do
+    echo -n "."
+    sleep 0.5
 done
+echo "started!"
 
 # Push the rancher-server IP to the rancher-agent ( dirty patch until it's officially fixed )
+echo ">> Creating the Unit to link Rancher Agent to Rancher Server locally..."
 echo "[Unit]
 Description=Patch Ranger Agent Hosts with Rancher Server IP ( dirty way to link them )
 After=docker.service
@@ -64,22 +79,24 @@ Requires=docker.service
 
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/bash -c \"/usr/bin/echo \\\"\$(/usr/bin/docker inspect --format '{{ .NetworkSettings.IPAddress }}' rancher-server) rancher-server\\\" >> /var/lib/docker/containers/\$(/usr/bin/docker inspect --format '{{ .Id }}' rancher-agent)/hosts\"
+ExecStart=/usr/bin/bash -c \"/usr/bin/echo \\\"\$(/usr/bin/docker inspect --format '{{ .NetworkSettings.IPAddress }}' $RANCHER_SERVER_NAME) $RANCHER_SERVER_NAME\\\" >> /var/lib/docker/containers/\$(/usr/bin/docker inspect --format '{{ .Id }}' $RANCHER_AGENT_NAME)/hosts\"
 
 [Install]
-WantedBy=multi-user.target" >> /etc/systemd/system/patch-rancher-agent-hosts.service
+WantedBy=multi-user.target" > /etc/systemd/system/patch-rancher-agent-hosts.service
 # Do it on every reboot
-systemctl enable patch-rancher-agent-hosts.service
+systemctl enable patch-rancher-agent-hosts.service &>/dev/null
 # And do it now please :)
-systemctl start patch-rancher-agent-hosts.service
+systemctl start patch-rancher-agent-hosts.service &>/dev/null
 
 # Prepare the Mail Server data folder
-mkdir -p /srv/mail
+echo ">> Creating /srv/mail folder..."
+mkdir -p /srv/mail &>/dev/null
 
 # Install the Mail Server
+echo ">> Running Mail server..."
 docker run \
     -d \
-    --name=mail-server \
+    --name="$MAILSERVER_NAME" \
     --restart=always \
     --expose=80 \
     --expose=443 \
@@ -95,14 +112,19 @@ docker run \
     -e "VIRTUAL_HOST=$MAILSERVER_DOMAIN" \
     -e "VIRTUAL_PROTO=https" \
     -e "VIRTUAL_PORT=443" \
-    analogic/poste.io
+    analogic/poste.io &>/dev/null
 
 # Wait until the mail server is up and running
-until [ "$(/usr/bin/docker inspect -f {{.State.Running}} mail-server)"=="true" ]; do
-    sleep 0.1
+echo -n ">> Waiting for Mail server to start..."
+while [ ! $(docker top $MAILSERVER_NAME &>/dev/null && echo $?) ]
+do
+    echo -n "."
+    sleep 0.5
 done
+echo "started!"
 
 # Print friendly done message
-echo "All right! Everything seems to be installed correctly.\n
-It's truly suggested to reboot now your system to get everything up and running.\n
-Have a nice day!"
+echo "-----------------------------------------------------"
+echo "All right! Everything seems to be installed correctly. It's truly suggested to reboot now your system to get everything up and running."
+echo "Have a nice day!"
+echo "-----------------------------------------------------"
