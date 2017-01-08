0608e484842040078309cebd10a7b9cc1887ed4d
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-01-08 22:38:21 +0100

    Add Cronjob time environment variable

--- modules/lineage_cicd_ota.sh
+++ modules/lineage_cicd_ota.sh
@@ -10,6 +10,7 @@ USER_MAIL="john.doe@awesome.email"
 DEVICE_LIST="hammerhead" # See https://github.com/julianxhokaxhiu/docker-lineage-cicd/blob/master/Dockerfile#L22
 BRANCH_NAME="cm-14.1" # See https://github.com/LineageOS/android_vendor_cm/branches
 OTA_URL="https://$LINOTA_DOMAIN/api" # See https://blog.julianxhokaxhiu.com/how-the-cm-ota-server-works-and-how-to-implement-and-use-ours/
+CRONTAB_TIME="0 10 * * *" # To make it easy, use https://crontab.guru/
 
 # Prepare the Lineage OTA data folder
 echo ">> Creating /srv/data/$LINOTA_NAME folder..."
@@ -51,6 +52,7 @@ docker run \
     -e "DEVICE_LIST=$DEVICE_LIST" \
     -e "BRANCH_NAME=$BRANCH_NAME" \
     -e "OTA_URL=$OTA_URL" \
+    -e "CRONTAB_TIME=$CRONTAB_TIME" \
     -v "/srv/data/$LINCICD_NAME/ccache:/srv/ccache" \
     -v "/srv/data/$LINCICD_NAME/src:/srv/src" \
     -v "/srv/data/$LINOTA_NAME:/srv/out" \
