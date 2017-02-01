7ffade2ae4f02dbf36df2c2a198476139d2cf0ce
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-02-01 18:08:40 +0100

    Add support for the newly introduced flags

--- modules/dns_server.sh
+++ modules/dns_server.sh
@@ -6,6 +6,8 @@ DNSSERVER_NAME="dns-server"
 LETSENCRYPT_EMAIL="foo@bar.mail"
 CUSTOM_DNS="8.8.8.8;8.8.4.4;[2001:4860:4860::8888];[2001:4860:4860::8844]"
 API_KEY=""
+CRONTAB_TIME="0 10 * * *"
+ENABLE_ADBLOCK=false
 
 # Prepare the DNS Server data folder
 echo ">> Creating /srv/data/$DNSSERVER_DOMAIN folder..."
@@ -39,6 +41,8 @@ docker run \
     --restart=always \
     -e "API_KEY=$API_KEY" \
     -e "CUSTOM_DNS=$CUSTOM_DNS" \
+    -e "CRONTAB_TIME=$CRONTAB_TIME" \
+    -e "ENABLE_ADBLOCK=$ENABLE_ADBLOCK" \
     -e "VIRTUAL_HOST=$DNSSERVER_DOMAIN" \
     -e "VIRTUAL_PORT=8080" \
     -e "LETSENCRYPT_HOST=$DNSSERVER_DOMAIN" \
