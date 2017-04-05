31db909d2de0c02eaf7b956171e5fc4f51acc7f0
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-04-05 23:41:18 +0200

    Move to the official ampache docker
    Slim down paths and configurations

--- modules/ampache.sh
+++ modules/ampache.sh
@@ -8,26 +8,7 @@ LETSENCRYPT_EMAIL="foo@bar.mail"
 # Prepare the ampache data folders
 echo ">> Creating /srv/data/$AMPACHE_DOMAIN folders..."
 mkdir -p "/srv/http/$AMPACHE_DOMAIN/config" &>/dev/null
-mkdir -p "/srv/http/$AMPACHE_DOMAIN/themes" &>/dev/null
 mkdir -p "/srv/dbs/$AMPACHE_DOMAIN" &>/dev/null
-
-# Run the database docker.
-# This will be executed only once, when the Database docker doesn't exist,
-# else will just "complain" that the docker already exists,
-# which is what we want. One database docker per VM.
-echo ">> Creating Database docker ( only if it doesn't already exist )..."
-docker run \
-    -d \
-    --restart=always \
-    --name="$AMPACHE_DOMAIN-db" \
-    -e "MYSQL_DATABASE=ampache" \
-    -e "MYSQL_USER=ampache" \
-    -e "MYSQL_PASSWORD=password" \
-    -e "MYSQL_ROOT_PASSWORD=root" \
-    -v "/srv/dbs/$AMPACHE_DOMAIN:/var/lib/mysql" \
-    mariadb \
-    --character-set-server=utf8 \
-    --collation-server=utf8_unicode_ci &>/dev/null
 
 # Install ampache
 echo ">> Running ampache..."
@@ -35,14 +16,13 @@ docker run \
     -d \
     --name="$AMPACHE_NAME" \
     --restart=always \
-    --link="$AMPACHE_DOMAIN-db:db" \
     -e "VIRTUAL_HOST=$AMPACHE_DOMAIN" \
     -e "LETSENCRYPT_HOST=$AMPACHE_DOMAIN" \
     -e "LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL" \
-    -v "/PATH/TO/MUSIC/ON/THE/HOST:/var/data:ro" \
+    -v "/PATH/TO/MUSIC/ON/THE/HOST:/media:ro" \
+    -v "/srv/dbs/$AMPACHE_DOMAIN:/var/lib/mysql" \
     -v "/srv/http/$AMPACHE_DOMAIN/config:/var/www/html/config" \
-    -v "/srv/http/$AMPACHE_DOMAIN/themes:/var/www/html/themes" \
-    plusminus/ampache &>/dev/null
+    ampache/ampache &>/dev/null
 
 # Wait until the docker is up and running
 echo -n ">> Waiting for ampache to start..."
